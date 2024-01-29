538a461027857e714d49c2df9d4816ee0db1b856
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-01-29 11:30:39 +0530

    Update ReverseString.java

--- JAVA Codes/ReverseString.java
+++ JAVA Codes/ReverseString.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class ReverseString {
 	public static void main(String[] args) {
-
+		String str, NullStr="";
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter String to Reverse : ");
+		str=sc.nextLine();
+		char ch;
+		System.out.println("\nOriginal String = "+str);
+		for(int i=0; i<str.length(); i++) {
+			ch=str.charAt(i);
+			NullStr=ch+NullStr;
+		}
+		System.out.println("\nReversed String = "+NullStr);
 	}
 }
