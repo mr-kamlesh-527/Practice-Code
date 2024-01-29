88a1d5be48742b4ae044d309f7139a0d2f01d006
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-01-30 01:43:58 +0530

    Update Ascii.java

---
+++ JAVA Codes/Ascii.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ascii {
+    public static void main(String[] args) {
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Charector : ");
+        ch = sc.next().charAt(0);
+        int a = ch;
+        System.out.println("Ascii value of "+ch+" is = "+a);
+
+    }
+
+}
+
+
