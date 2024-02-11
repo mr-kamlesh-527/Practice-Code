7549d374eaaf8503633b7c37c7bc6e8cd2ffc83c
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-02-11 10:36:37 +0530

    Update Armstrong.java

--- JAVA Codes/Armstrong.java
+++ JAVA Codes/Armstrong.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+class Armstrong{
+    public static void main(String[] args) {
+        int n, arm=0, rem=0, c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number ");
+        n = sc.nextInt();
+        c=n;
+        while(n>0){
+            rem=n%10;
+            arm=(rem*rem*rem)+arm;
+            n = n/10;
+        }
+        if(c==arm){
+            System.out.println(c+" is an armstrong number");
+        }
+        else{
+            System.out.println(c+" is not armstrong number ");
+        }
+        
+    }
+}
