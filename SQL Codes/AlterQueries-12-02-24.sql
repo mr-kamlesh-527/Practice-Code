0acd393af2a8fb6f7683b009a7b583da56bc3d3c
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-02-12 11:43:57 +0530

    Create AlterQueries-12-02-24.sql

---
+++ SQL Codes/AlterQueries-12-02-24.sql
@@ -0,0 +1,6 @@
+---alter query----
+alter table emp add eadress varchar(50);
+---update query---- 
+update emp1 set ename='mr ck' where eid=1;
+--select query----
+ select ename from emp where eid=1;
